8060c78ac2230d8a00e216e7def44fd83b3fc05b
reona5 <leonardooooo55555@gmail.com>
2019-07-17 20:26:00 +0900

    implement event creation

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,11 @@ const events = (state = [], action) => {
     case 'CREATE_EVENT':
       const event = { title: action.title, body: action.body };
       const length = state.length;
-      const id = length === 0 ? (id = 1) : (id = state[length - 1].id + 1);
+      const id = length === 0 ? 1 : state[length - 1].id + 1;
       return [...state, { id, ...event }];
 
     case 'DELETE_EVENT':
-      const event = {};
+      return state;
     case 'DELETE_ALL_EVENT':
       return [];
     default:
