2b92d405dcbc548f0c9b41902814693b15af00bd
reona5 <leonardooooo55555@gmail.com>
2019-07-18 16:52:20 +0900

    refactor events with reducer

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
-import React, { useReducer } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useReducer } from 'react';
 import EventForm from './EventForm';
 import Events from './Events';
 import AppContext from '../contexts/AppContext';
 import reducer from '../reducers';
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, []);
+  const initialState = {
+    events: []
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <AppContext.Provider value={{ state, dispatch }}>
