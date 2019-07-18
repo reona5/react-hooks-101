8700a5b0cc3a551f811a4a2b35cd27231bd1b869
reona5 <leonardooooo55555@gmail.com>
2019-07-18 15:04:36 +0900

    replace type names with constants

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { CREATE_EVENT, DELETE_ALL_EVENT } from '../actions';
 
 const EventForm = ({ state, dispatch }) => {
   const [title, setTitle] = useState('');
@@ -7,7 +8,7 @@ const EventForm = ({ state, dispatch }) => {
   const addEvent = e => {
     e.preventDefault();
     dispatch({
-      type: 'CREATE_EVENT',
+      type: CREATE_EVENT,
       title,
       body
     });
@@ -23,7 +24,7 @@ const EventForm = ({ state, dispatch }) => {
     );
     if (result)
       dispatch({
-        type: 'DELETE_ALL_EVENT'
+        type: DELETE_ALL_EVENT
       });
   };
 
