8f3349e196fcff17bc08948a9e962606d680b97f
reona5 <leonardooooo55555@gmail.com>
2019-07-22 14:16:51 +0900

    create operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,6 +1,13 @@
 import React, { useContext, useState } from 'react';
-import { CREATE_EVENT, DELETE_ALL_EVENT } from '../actions';
+import {
+  CREATE_EVENT,
+  DELETE_ALL_EVENTS,
+  ADD_OPERATION_LOG,
+  DELETE_OPERATION_LOGS,
+  DELETE_ALL_OPERATION_LOGS
+} from '../actions';
 import AppContext from '../contexts/AppContext';
+import { timeCurrentIso8601 } from '../utils';
 
 const EventForm = () => {
   const { state, dispatch } = useContext(AppContext);
@@ -15,19 +22,31 @@ const EventForm = () => {
       body
     });
 
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: 'イベントを作成しました。',
+      operatedAt: timeCurrentIso8601()
+    });
+
     setTitle('');
     setBody('');
   };
 
-  const deleteAllEvent = e => {
+  const deleteAllEvents = e => {
     e.preventDefault();
     const result = window.confirm(
       '全てのイベントを本当に削除しても良いですか？'
     );
     if (result)
       dispatch({
-        type: DELETE_ALL_EVENT
+        type: DELETE_ALL_EVENTS
       });
+
+    dispatch({
+      type: DELETE_ALL_OPERATION_LOGS,
+      description: '全てのイベントを削除しました。',
+      operatedAt: timeCurrentIso8601
+    });
   };
 
   const unCreatable = title === '' || body === '';
@@ -65,7 +84,7 @@ const EventForm = () => {
         </button>
         <button
           className="btn btn-danger"
-          onClick={deleteAllEvent}
+          onClick={deleteAllEvents}
           disabled={state.events.length === 0}
         >
           全てのイベントを削除する
